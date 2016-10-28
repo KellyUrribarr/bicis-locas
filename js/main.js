fd52b0fb6820bce2d3a56d1adc75084accb65920
kellyurribarr <kellyurribarri@gmail.com>
2016-10-28 12:14:11 -0300

    indentacion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-
-/*document.getElementById("form-signup").submit();*/
-
-//NOMBRE
 function validateForm(){
   var name = document.forms[0]["name"].value;
   var lastname = document.forms[0]["lastname"].value;
@@ -9,64 +5,72 @@ function validateForm(){
   var password = document.forms[0]["input-password"].value;
   var seleccion = document.querySelector('select').selectedIndex;
   var twitter = document.forms[0]["input-social"].value;
+  var check = document.getElementsByName('seleccionar[]');
 
-//comprobar que el campo este completo
-   if (name == null || name.length == 0 || /^\s+$/.test(name)) {
-       alert("nombre o apellido son campo obligatorio");
-       return false;
-   }
-//comprobar que solo sean letras
-  else if(/^[a-zA-Z]+$/.test(name) == false){
-  alert("nombre no valido ingresar solo letras");
-  return true;
-  }
-//comprobar que el primer caracter es mayuscula
-  else if(name.charAt(0).toUpperCase() !== name.charAt(0)){
-  alert("la primera letra de nombre y apellido tiene que ser mayuscula");
-  return false;
-  }
-
-  //APELLIDO
-  //comprobar que el campo este completo
-   if (lastname == null || lastname.length == 0 || /^\s+$/.test(lastname)) {
-       alert("nombre es un campo obligatorio");
-       return false;
-   }
-//comprobar que solo sean letras
-  else if(/^[a-zA-Z]+$/.test(lastname) == false){
-  alert("apellido no valido ingresar solo letras");
-  return true;
-  }
-//comprobar que el primer caracter es mayuscula
-  else if(lastname.charAt(0).toUpperCase() !== lastname.charAt(0)){
-  alert("la primera letra de nombre y apellido tiene que ser mayuscula");
-  return false;
-  }
-
-//CORREO ELECTRONICO
-  if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email) == false){
-        alert("La dirección de email es incorrecta.");
-     }
-
-//CONTRASEÑA
-  if (password == null || password.length == 0) {
-       alert("contraseña campo obligatorio");
-       return false;
-   }
-   else if(password == "password" || password == 123456 || password == 098754){
-    alert("campo incorrecto");
+////NOMBRE
+  if (name == null || name.length == 0 || /^\s+$/.test(name)) {
+    alert("nombre o apellido son campo obligatorio");
     return false;
    }
-   else if(password.length < 6){
-    alert("tu contraseña debe ser mayor a 6 caracteres");
+//COMPROBAR QUE SOLO SEAN LETRAS
+  else if (/^[a-zA-Z]+$/.test(name) == false){
+    alert("nombre no valido ingresar solo letras");
+    return true;
+  }
+//COMPROBAR QUE EMPIECE CON MAYUSCULAS
+  else if (name.charAt(0).toUpperCase() !== name.charAt(0)){
+    alert("la primera letra de nombre y apellido tiene que ser mayuscula");
+    return false;
+  }
+//APELLIDO
+   if (lastname == null || lastname.length == 0 || /^\s+$/.test(lastname)) {
+    alert("nombre es un campo obligatorio");
+    return false;
    }
-  //SELECCION 
-
-if( seleccion == null || seleccion == 0 ) {
-  alert("por favor ingrese su bici");
-  return false;
+//COMPROBAR QUE SOLO SEAN LETRAS
+  else if (/^[a-zA-Z]+$/.test(lastname) == false){
+    alert("apellido no valido ingresar solo letras");
+    return true;
+  }
+//COMPROBAR QUE EMPIECE CON MAYUSCULAS
+  else if (lastname.charAt(0).toUpperCase() !== lastname.charAt(0)){
+    alert("la primera letra de nombre y apellido tiene que ser mayuscula");
+    return false;
+  }
+//CORREO ELECTRONICO
+  if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email) == false){
+    alert("La dirección de email es incorrecta.");
+    }
+//CONTRASEÑA
+  if (password == null || password.length == 0) {
+    alert("contraseña campo obligatorio");
+    return false;
+   }
+   else if (password == "password" || password == 123456 || password == 098754){
+    alert("campo incorrecto, los caracteres no puedes ser igual a 123456, 098754, password");
+    return false;
+   }
+   else if (password.length < 6){
+     alert("tu contraseña debe ser mayor a 6 caracteres");
+   }
+//SELECCION 
+  if( seleccion == null || seleccion == 0 ) {
+    alert("por favor ingrese un tipo de bici");
+    return false;
+    }
+//TWITTER
+  if(/^[a-zA-Z0-9- ]*$/.test(twitter) == false) {
+    alert('contiene caracteres no válidos. ejemplo @');
+    }
+  else if (twitter == null || twitter.length == 0 || /^\s+$/.test(twitter)) {
+    alert("twitter es un campo obligatorio");
+    return false;
+}
+//CHECKBOX
 }
 
-}
 
 
+
+
+    
